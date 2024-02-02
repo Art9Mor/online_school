7541e65efd57ab3bb77bb7bec645fc8f1607cd7f
Artem <deusestmaleficus@gmail.com>
2024-02-02 11:17:14 +0700

    auto fill

---
+++ users/management/commands/fill_payment.py
@@ -0,0 +1,23 @@
+from django.core.management import BaseCommand
+
+from learning.models import Lesson, Course
+from users.models import User, Payment
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        payment_to_create = []
+        for user in User.objects.all():
+            payment_to_create.append(Payment(user=user,
+                                             paid_lesson=Lesson.objects.get(pk=1),
+                                             paid_sum=10000,
+                                             method='Cash'))
+        for user in User.objects.all():
+            payment_to_create.append(Payment(user=user,
+                                             paid_course=Course.objects.get(pk=1),
+                                             paid_sum=100000,
+                                             method='Transfer'))
+
+        Payment.objects.bulk_create(payment_to_create)
+
