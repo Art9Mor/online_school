693c69ea8b526bce4bbd28afcd8a558eb1477c69
Artem <deusestmaleficus@gmail.com>
2024-05-21 21:35:15 +0700

    Remove HOST from DATABASES

--- config/settings.py
+++ config/settings.py
@@ -96,7 +96,6 @@ DATABASES = {
         'NAME': os.getenv('DATABASE_NAME'),
         'USER': os.getenv('DATABASE_USER'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': os.getenv('DATABASE_HOST', 'localhost')
     }
 }
 
